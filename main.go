8117d4f9e1a194a022e6b4fab87e9125d3d2c542
SimpleAstronaut <escscience@163.com>
2022-12-20 10:15:06 +0800

    添加登录接口

--- main.go
+++ main.go
@@ -1,3 +1,12 @@
+/*
+ * The Saturn Project
+ * S-Blog v2.0
+ * based on golang Gin
+ * Author: SimpleAstronaut
+ * Version: v1.0.0_alpha_22122001
+ * 2022-12-20
+ */
+
 package main
 
 import (
@@ -8,8 +17,23 @@ import (
 func main() {
 	r := gin.Default()
 	r.LoadHTMLGlob("public/*")
+
+	//主页面测试路由
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	//登录获取token接口
+	r.GET("/login", func(c *gin.Context) {
+		username := c.DefaultQuery("username", "null")
+		password := c.DefaultQuery("password", "null")
+
+		//判断username和password是否为空
+		if username == "null" || password == "null" {
+			c.HTML(http.StatusOK, "err.html", gin.H{
+				"errmsg": "Username or Password is null",
+			})
+		}
 	})
 
 	err := r.Run(":9000")
