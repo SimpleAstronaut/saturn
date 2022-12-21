5b20faa2a9bafa6a8491f910954f8c6ece7fc37f
SimpleAstronaut <escscience@163.com>
2022-12-21 12:01:25 +0800

    获取blog接口

--- api/get.go
+++ api/get.go
@@ -1,5 +1,38 @@
+// Package api get module
+// return page or blog
+// version 1.0.1
+// author SimpleAstronaut
+// 2022-12-21
+
 package api
 
-func main() {
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+)
 
+type Blog struct {
+	title  string
+	author string
+	time   string
+	blog   string
+}
+
+func Get(mode, name string) {
+	//获取blog接口
+	if mode == "blog" {
+		blogPath := "./blog/" + name + ".json"
+		file, err := ioutil.ReadFile(blogPath)
+		var b Blog
+		if err != nil {
+			fmt.Printf("文件打开失败 [Err:%s]\n", err.Error())
+			return
+		}
+		err = json.Unmarshal(file, &b)
+		if err != nil {
+			log.Fatal("Error during Unmarshal(): ", err)
+		}
+	}
 }
